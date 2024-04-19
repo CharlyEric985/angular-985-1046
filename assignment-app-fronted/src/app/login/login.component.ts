99602d91a36ca0739768139d5c7ee185ccc8aa7b
TheoGuillaume <81827200+TheoGuillaume@users.noreply.github.com>
2024-04-19 21:06:12 +0300

    ajustement spinner login

--- assignment-app-fronted/src/app/login/login.component.ts
+++ assignment-app-fronted/src/app/login/login.component.ts
@@ -7,11 +7,12 @@ import {MatButtonModule} from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule,MatInputModule,MatButtonModule,CommonModule,ReactiveFormsModule],
+  imports: [MatCardModule,MatInputModule,MatButtonModule,CommonModule,ReactiveFormsModule,MatProgressSpinnerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -21,22 +22,30 @@ export class LoginComponent {
     mdp: new FormControl('123456'),
   });
   error: string | null | undefined;
+  isLoading: boolean = false; // Ajout de la propriété pour gérer l'état du spinner
+
 
   constructor(private authService: AuthService,private router: Router) {} // Injectez le service AuthService
 
  
   submit() {
+    this.isLoading = true; // Commencez par activer le spinner
     const { email, mdp } = this.form.value;
-    this.authService.login(email, mdp).subscribe(
-      (response) => {
-        // Connexion réussie
-        this.router.navigateByUrl('/acceuil');
-      },
-      (error: string) => {
-        // Gestion de l'erreur de connexion
-        this.error = error;
-      }
-    );
-  
+    
+    // Utilisez setTimeout pour ajouter un délai avant que le spinner n'apparaisse
+    setTimeout(() => {
+      this.authService.login(email, mdp).subscribe(
+        (response) => {
+          // Connexion réussie
+          this.router.navigateByUrl('/acceuil');
+        },
+        (error: string) => {
+          // Gestion de l'erreur de connexion
+          this.error = error;
+        }
+      ).add(() => {
+        this.isLoading = false; // Assurez-vous d'arrêter le spinner que la requête soit réussie ou non
+      });
+    }, 2000); // 2000ms (2 secondes) de délai avant que le spinner n'apparaisse
   }
 }
